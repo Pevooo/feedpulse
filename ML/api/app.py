ce9b7d45ac34b6840c2d66bb0a683058fb910bab
Pavly <pavlysamuelatef@gmail.com>
2024-09-20 01:32:38 +0300

    Modified Project structure and Created Model.py

---
+++ ML/api/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index() -> str:
+    match request.method:
+        case 'GET':
+            # Display the form
+            return render_template('test_models.html')
+        case 'POST':
+            # Get form details
+            question = request.form.get("question")
+            answer = request.form.get("answer")
+            candidate_answer = request.form.get("candidate_answer")
+
+            return render_template('test/test_models.html')
+
---
+++ ML/constants.py
@@ -0,0 +1,5 @@
+"""
+This file should contain all project constants
+"""
+
+MODEL_SAVE_PATH = '/models'
