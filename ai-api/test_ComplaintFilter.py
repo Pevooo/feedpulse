ba41a186daedf7b31a93a20aa48ca9266055fa50
Pavly <99624292+Pevooo@users.noreply.github.com>
2024-10-03 23:13:41 +0300

    Add Complaint Filter (#10)

---
+++ ai-api/test_ComplaintFilter.py
@@ -0,0 +1,19 @@
+import unittest
+from ComplaintFilter import ComplaintFilter
+
+
+class TestComplaintFiler(unittest.TestCase):
+    def test_wrap_text(self):
+        complaint_filter = ComplaintFilter()
+        self.assertEqual(
+            complaint_filter.wrap_text("hello"),
+            "You will be provided with some text and you have to tell if it's a complaint or not using only one word (YES or NO).\nHere is the text: \"hello\"."
+        )
+
+    def test_service_connection_complaint(self):
+        complaint_filter = ComplaintFilter()
+        self.assertTrue(complaint_filter("This place is really bad"))
+
+    def test_service_connection_not_complaint(self):
+        complaint_filter = ComplaintFilter()
+        self.assertFalse(complaint_filter("This place is really good"))
