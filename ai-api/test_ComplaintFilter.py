1112f0b7622e07c3a81a5efe4e3afb1d8cdaba27
Pevooo <pavlysamuelatef@gmail.com>
2024-10-03 19:03:35 +0300

    Update Pipelines and added test

---
+++ ai-api/test_ComplaintFilter.py
@@ -0,0 +1,14 @@
+import unittest
+from ComplaintFilter import ComplaintFilter
+
+class TestComplaintFiler(unittest.TestCase):
+    
+    def test_wrap_text(self):
+        complaint_filter = ComplaintFilter()
+        self.assertEqual(
+            complaint_filter.__wrap_text("hello"),
+            f"""
+            You will be some text and you have to tell if it's a complaint or not using only one word (YES or NO).
+            Here is the text: "hello".
+            """,
+        )
