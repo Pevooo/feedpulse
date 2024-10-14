8f1ed578483e4cdf9f0b47078d3f4b73a59a8a19
Pavly <pavlysamuelatef@gmail.com>
2024-10-14 14:45:48 +0300

    Removed csharp from local runner and added coloring

--- util/local_runner.py
+++ util/local_runner.py
@@ -1,7 +1,7 @@
 import subprocess
 import threading
 import os
-import webbrowser
+from colorama import init, Fore, Style
 
 # Run this script if you want to build, test and run the project locally
 
@@ -9,15 +9,13 @@ AI_API_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(
 
 
 def main() -> None:
-    print("Running...")
+    init()
+    print(Fore.BLUE + "Running..." + Style.RESET_ALL)
     ai_api_thread = threading.Thread(target=run_ai_api)
-    app_api_thread = threading.Thread(target=run_app_api)
 
     ai_api_thread.start()
-    app_api_thread.start()
 
     ai_api_thread.join()
-    app_api_thread.join()
 
 
 def run_command(command: str) -> subprocess.CompletedProcess[str]:
@@ -25,19 +23,15 @@ def run_command(command: str) -> subprocess.CompletedProcess[str]:
 
 
 def install_dependancies() -> None:
+    print(Fore.BLUE + "Installing Dependancies..." + Style.RESET_ALL)
     run_command(f"pip install -r \"{os.path.join(AI_API_DIRECTORY, 'requirements.txt')}\"")
+    print(Fore.GREEN + "Dependancies Installed!" + Style.RESET_ALL)
 
 
 def run_ai_api() -> None:
     install_dependancies()
-    webbrowser.open_new("http://127.0.0.1:5000")
-    print("AI API URL: http://127.0.0.1:5000")
-    result = run_command(f"python \"{os.path.join(AI_API_DIRECTORY, 'app.py')}\"")
-    print(result.stdout)
-
-
-def run_app_api() -> None:
-    print("TODO: CREATE APP API AND ADD RUN COMMANDS")
+    print(Fore.GREEN + "AI API URL: http://127.0.0.1:5000" + Style.RESET_ALL)
+    run_command(f"python \"{os.path.join(AI_API_DIRECTORY, 'app.py')}\"")
 
 
 if __name__ == "__main__":
