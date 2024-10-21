02aa401139a9c4fbbcdc8662603d01e5de2f6ae9
Pevooo <pavlysamuelatef@gmail.com>
2024-10-21 21:11:58 +0300

    Add C# Support

--- util/local_runner.py
+++ util/local_runner.py
@@ -6,22 +6,43 @@ import subprocess
 import threading
 import os
 import webbrowser
+from colorama import Style, Fore
 
 AI_API_DIRECTORY = os.path.abspath(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "ai-api")
 )
 
+WEB_API_DIRECTORY = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "web-api")
+)
+
+
+def print_error(text: str) -> None:
+    print(Fore.RED + text + Style.RESET_ALL)
+
+
+def print_pending_process(text: str) -> None:
+    print(Fore.BLUE + text + Style.RESET_ALL)
+
+
+def print_success(text: str) -> None:
+    print(Fore.GREEN + text + Style.RESET_ALL)
+
+
+def print_warning(text: str) -> None:
+    print(Fore.YELLOW + text + Style.RESET_ALL)
+
 
 def main() -> None:
-    print("Running...")
+    print_pending_process("Running...")
     ai_api_thread = threading.Thread(target=run_ai_api)
-    app_api_thread = threading.Thread(target=run_app_api)
+    web_api_thread = threading.Thread(target=run_app_api)
 
     ai_api_thread.start()
-    app_api_thread.start()
+    web_api_thread.start()
 
     ai_api_thread.join()
-    app_api_thread.join()
+    web_api_thread.join()
 
 
 def run_command(command: str) -> subprocess.CompletedProcess[str]:
@@ -31,21 +52,37 @@ def run_command(command: str) -> subprocess.CompletedProcess[str]:
 
 
 def install_dependancies() -> None:
+    print_pending_process("AI-API: Installing Dependancies...")
     run_command(
         f"pip install -r \"{os.path.join(AI_API_DIRECTORY, 'requirements.txt')}\""
     )
+    print_success("AI-API: Dependancies Installed Successfully!")
 
 
 def run_ai_api() -> None:
     install_dependancies()
     webbrowser.open_new("http://127.0.0.1:5000")
-    print("AI API URL: http://127.0.0.1:5000")
+    print("AI-API: Listen URL: http://127.0.0.1:5000")
+    print_warning("AI-API: The api may be not working")
     result = run_command(f"python \"{os.path.join(AI_API_DIRECTORY, 'app.py')}\"")
-    print(result.stdout)
+    if result.stderr:
+        print_error(result.stderr)
 
 
 def run_app_api() -> None:
-    print("TODO: CREATE APP API AND ADD RUN COMMANDS")
+    print_pending_process("WEB-API: Building...")
+    build_result = run_command(f'dotnet build "{WEB_API_DIRECTORY}"')
+    if build_result.stderr:
+        print_error(build_result)
+    else:
+        print_success("WEB-API: Build Successful!")
+
+    print_pending_process("WEB-API: Running...")
+    webbrowser.open_new("http://127.0.0.1:5144")
+    print("WEB-API: Listen URL: http://127.0.0.1:5144")
+    run_result = run_command(f'dotnet run --project "{WEB_API_DIRECTORY}"')
+    if run_result.stderr:
+        print_error(run_result)
 
 
 if __name__ == "__main__":
