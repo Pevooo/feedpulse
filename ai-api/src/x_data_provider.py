54be7194082c9bfb2a71bfe48b1d7b4e57f07aa4
John Ashraf <jone.ashraf699@gmail.com>
2024-10-27 13:09:09 +0300

    Add X code for setup

---
+++ ai-api/src/x_data_provider.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+import twikit
+import time
+from random import randint
+
+
+class Tweet:
+    def __init__(self, tweet: twikit.Tweet) -> None:
+        self.text: str = tweet.text
+        self.id: str = tweet.id
+        self.user: str = tweet.user.name
+        self.date: datetime = tweet.created_at_datetime
+        self.replies: list[Tweet] = (
+            list(map(Tweet, tweet.replies)) if tweet.replies else None
+        )
+
+
+class XDataProvider:
+    def __init__(self, username: str, email: str, password: str) -> None:
+        self.client = twikit.Client("en-US")
+
+    async def login(self, username: str, email: str, password: str):
+        await self.client.login(
+            auth_info_1=username,
+            auth_info_2=email,
+            password=password,
+        )
+
+    async def get_tweets(self, num_tweets: int, query: str) -> list[twikit.Tweet]:
+
+        tweets = await self.client.search_tweet(query, "Latest")
+        counts = 0
+
+        all_tweets = []
+
+        for tweet in tweets:
+            counts += 1
+            all_tweets.append(Tweet(tweet))
+
+        # Search more tweets
+        while counts < num_tweets:
+            wait_time = randint(5, 12)
+            time.sleep(wait_time)
+            more_tweets = await tweets.next()
+            for tweet in more_tweets:
+                all_tweets.append(Tweet(tweet))
+        return all_tweets
