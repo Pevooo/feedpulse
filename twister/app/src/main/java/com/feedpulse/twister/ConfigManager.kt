40bfbeaff7975c455648e690d42dc1fd21fac961
Pevooo <pavlysamuelatef@gmail.com>
2024-11-09 20:10:10 +0200

    Add twister

---
+++ twister/app/src/main/java/com/feedpulse/twister/ConfigManager.kt
@@ -0,0 +1,83 @@
+package com.feedpulse.twister
+
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class ConfigManager(private val url: URL) {
+
+    private var cachedConfig: Config? = null;
+
+    fun applyChanges(data: Config) {
+
+        val gson = Gson()
+        val postData = gson.toJson(data)
+
+        val connection = (url.openConnection() as HttpURLConnection).apply {
+            requestMethod = "POST"
+            setRequestProperty("Content-Type", "application/json")
+            doOutput = true
+        }
+
+        OutputStreamWriter(connection.outputStream).also {
+            it.write(postData)
+            it.flush()
+            it.close()
+        }
+
+    }
+
+    fun getCurrentConfig(): Config {
+        if (cachedConfig != null) {
+            return cachedConfig!!
+        }
+
+        cachedConfig = Config(
+            mutableListOf(
+                Setting("Setting 1", "true"),
+                Setting("Setting 3", "false"),
+                Setting("Setting 4", "one"),
+            )
+        )
+
+        return cachedConfig!!
+
+        return try {
+            val connection = (url.openConnection() as HttpURLConnection)
+                .apply {
+                    requestMethod = "GET"
+                }.also {
+                    it.connect()
+                }
+
+
+            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                val text = connection.inputStream.bufferedReader().use { it.readText() }
+                val listType = object : TypeToken<MutableList<Setting>>() {}.type
+                val settings: MutableList<Setting> = Gson().fromJson(text, listType)
+
+                Config(settings)
+            } else {
+                Config(mutableListOf())
+            }
+        } catch (e: Exception) {
+            Config(mutableListOf())
+        }
+    }
+}
+
+class Setting(
+    val settingName: String,
+    var settingValue: String,
+) {
+    val settingType = if (settingValue == "true" || settingValue == "false") "bool" else "selective"
+}
+
+class Config(
+    var settingsList: List<Setting>
+)
