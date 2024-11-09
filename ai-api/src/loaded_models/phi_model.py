ba58c2bb97e4673627fb520e2fe9b74263be98e8
Aya Abdellatif <ayaabdllatif112@gmail.com>
2024-11-09 15:26:57 +0200

    clean code

--- ai-api/src/loaded_models/phi_model.py
+++ ai-api/src/loaded_models/phi_model.py
@@ -2,20 +2,23 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import src.loaded_models.model as model
 
+
 class PhiModel(model.Model):
     def __init__(self) -> None:
-        self.__model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3-mini-128k-instruct",
-                                                            device_map="cuda" if torch.cuda.is_available() else "cpu",  
-                                                            torch_dtype="auto",  
-                                                            trust_remote_code=True,
+        self.__model = AutoModelForCausalLM.from_pretrained(
+            "microsoft/Phi-3-mini-128k-instruct",
+            device_map="cuda" if torch.cuda.is_available() else "cpu",
+            torch_dtype="auto",
+            trust_remote_code=True,
         )
-        self.__tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-128k-instruct")
-
-       
+        self.__tokenizer = AutoTokenizer.from_pretrained(
+            "microsoft/Phi-3-mini-128k-instruct"
+        )
 
     def generate_content(self, text: str, max_new_tokens: int = 100) -> str:
-        #tokenize the input
         inputs = self.__tokenizer(text, return_tensors="pt").to(self.__model.device)
-        outputs = self.__model.generate(inputs["input_ids"], max_new_tokens = max_new_tokens)
+        outputs = self.__model.generate(
+            inputs["input_ids"], max_new_tokens=max_new_tokens
+        )
         generated_text = self.__tokenizer.decode(outputs[0], skip_special_tokens=True)
         return generated_text
--- ai-api/src/topic_detection/topic_detector.py
+++ ai-api/src/topic_detection/topic_detector.py
@@ -1,11 +1,12 @@
 from src.topic_detection.topic_detector_result import TopicDetectorResult
 from src.loaded_models.model import Model
 
+
 class TopicDetector:
     def __init__(self, model: Model, org_topics: list) -> None:
         self.model = model
         self.org_topics = [topic.lower() for topic in org_topics]
-    
+
     def __call__(self, text: str) -> TopicDetectorResult:
         """
         This Method for detecting matching topics.
@@ -16,19 +17,18 @@ class TopicDetector:
         Returns:
             TopicDectorResult: Contains the list of detected topics.
         """
-        
+
         response: str = self.model.generate_content(self.wrap_text(text)).lower()
         topics = self.extract_topics(response)
-        #return topics
         return TopicDetectorResult(topics)
-    
+
     def extract_topics(self, response: str) -> list:
         detected_topics = [topic for topic in self.org_topics if topic in response]
         return detected_topics
-    
+
     def wrap_text(self, text: str) -> str:
         return (
-        "Identify and list only the relevant topics from the provided list that directly relate to the content in the text. "
-        f"The available topics are: {', '.join(self.org_topics)}.\n"
-        f"Here is the text:\n\"{text}\"."
-    )
+            "Identify and list only the relevant topics from the provided list that directly relate to the content in the text. "
+            f"The available topics are: {', '.join(self.org_topics)}.\n"
+            f'Here is the text:\n"{text}".'
+        )
