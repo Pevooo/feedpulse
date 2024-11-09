f31a6e69ea94415293932a2dba6b8510878e3039
Aya Abdellatif <ayaabdllatif112@gmail.com>
2024-11-09 23:42:18 +0200

    remove comments

--- ai-api/test/test_phi_model.py
+++ ai-api/test/test_phi_model.py
@@ -3,17 +3,8 @@ from src.loaded_models.phi_model import PhiModel
 
 
 class TestPhiModel(unittest.TestCase):
-    """
-    def test_connection(self):
-        try:
-            PhiModel()
-        except Exception as e:
-            self.fail(f"Connection Failed: {e}")
-    """
-
     def test_str_value(self):
         model = PhiModel()
 
         result = model.generate_content("The food taste was great")
-        # print(result)
         self.assertNotEqual(len(result), 0)
