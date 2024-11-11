2620ee8c263212c31a2205be1a625ecb1e1b0d5f
Pavly <99624292+Pevooo@users.noreply.github.com>
2024-11-11 22:44:23 +0200

    Improve util (#53)

--- util/local_runner.py
+++ util/local_runner.py
@@ -18,39 +18,16 @@ import subprocess
 import threading
 import os
 import webbrowser
-from colorama import Style, Fore
 
-AI_API_DIRECTORY = os.path.abspath(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "ai-api")
-)
+from constants import AI_API_DIRECTORY, WEB_API_DIRECTORY
 
-WEB_API_DIRECTORY = os.path.abspath(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "web-api")
-)
-
-
-def print_error(text: str) -> None:
-    print(Fore.RED + text + Style.RESET_ALL)
-
-
-def print_pending_process(text: str) -> None:
-    print(Fore.BLUE + text + Style.RESET_ALL)
-
-
-def print_success(text: str) -> None:
-    print(Fore.GREEN + text + Style.RESET_ALL)
-
-
-def print_warning(text: str) -> None:
-    print(Fore.YELLOW + text + Style.RESET_ALL)
-
-
-def print_important(text: str) -> None:
-    print(Fore.CYAN + text + Style.RESET_ALL)
+from util.printer import Printer
 
 
 def main() -> None:
-    print_pending_process("Running...")
+    Printer.init()
+    Printer.pending_process("Local Runner Running...")
+
     ai_api_thread = threading.Thread(target=run_ai_api)
     web_api_thread = threading.Thread(target=run_app_api)
 
@@ -68,51 +45,51 @@ def run_command(command: str) -> subprocess.CompletedProcess[str]:
 
 
 def build_ai_api() -> None:
-    print_pending_process("AI-API: Installing Dependencies...")
+    Printer.pending_process("AI-API: Installing Dependencies...")
     result = run_command(
         f"pip install -r \"{os.path.join(AI_API_DIRECTORY, 'requirements.txt')}\""
     )
 
     if result.returncode != 0:
-        print_error(result.stderr)
+        Printer.error(result.stderr)
     else:
-        print_success("AI-API: Dependencies Installed Successfully!")
+        Printer.success("AI-API: Dependencies Installed Successfully!")
 
 
 def run_ai_api() -> None:
     build_ai_api()
-    print_pending_process("AI-API: Running...")
+    Printer.pending_process("AI-API: Running...")
     webbrowser.open_new("http://127.0.0.1:5000")
-    print_important("AI-API: Listen URL: http://127.0.0.1:5000")
-    print_warning("AI-API: The api may be not working")
+    Printer.important("AI-API: Listen URL: http://127.0.0.1:5000")
+    Printer.warning("AI-API: The api may be not working")
     result = run_command(f"python \"{os.path.join(AI_API_DIRECTORY, 'app.py')}\"")
     if result.returncode != 0:
-        print_error(result.stderr)
+        Printer.error(result.stderr)
 
 
 def run_app_api() -> None:
     build_web_api()
-    print_pending_process("WEB-API: Running...")
+    Printer.pending_process("WEB-API: Running...")
     webbrowser.open_new("http://127.0.0.1:5144")
-    print_important("WEB-API: Listen URL: http://127.0.0.1:5144")
+    Printer.important("WEB-API: Listen URL: http://127.0.0.1:5144")
     result = run_command(f'dotnet run --project "{WEB_API_DIRECTORY}"')
     if result.returncode != 0:
-        print_error(result.stderr)
+        Printer.error(result.stderr)
 
 
 def build_web_api() -> None:
-    print_pending_process("WEB-API: Building...")
+    Printer.pending_process("WEB-API: Building...")
     build_result = run_command(f'dotnet build "{WEB_API_DIRECTORY}"')
     if build_result.returncode != 0:
-        print_error(build_result.stderr)
+        Printer.error(build_result.stderr)
     else:
-        print_success("WEB-API: Build Successful!")
+        Printer.success("WEB-API: Build Successful!")
 
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print_warning("Keyboard Interrupt Detected: Terminating...")
+        Printer.warning("Keyboard Interrupt Detected: Terminating...")
     except Exception as e:
-        print_error(str(e))
+        Printer.error(str(e))
