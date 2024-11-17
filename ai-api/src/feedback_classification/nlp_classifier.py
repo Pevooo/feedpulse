b7c3b1557a1001fa0a58cb5c63c1edf51c449395
Mollyamged <115906572+Mollyamged@users.noreply.github.com>
2024-11-17 18:59:33 +0200

    Add feedback classification using NLP (#71)

---
+++ ai-api/src/feedback_classification/nlp_classifier.py
@@ -0,0 +1,18 @@
+from transformers import pipeline
+from typing import Optional
+
+
+class NLPClassifier:
+    def __init__(self):
+        self.classifier = pipeline(
+            "sentiment-analysis",
+            model="nlptown/bert-base-multilingual-uncased-sentiment",
+        )
+
+    def __call__(self, text: str) -> Optional[bool]:
+        result = int(self.classifier(text)[0]["label"[0]])
+        if result < 3:
+            return False
+        elif result > 3:
+            return True
+        return None
