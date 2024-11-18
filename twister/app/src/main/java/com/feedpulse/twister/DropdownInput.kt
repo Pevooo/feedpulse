fba52fe56b1019e84eae7e08825e1dc37c8c548a
Pavly <pavlysamuelatef@gmail.com>
2024-11-18 23:13:47 +0200

    Fix Lint

--- twister/app/src/main/java/com/feedpulse/twister/DropdownInput.kt
+++ twister/app/src/main/java/com/feedpulse/twister/DropdownInput.kt
@@ -1,9 +1,19 @@
+package com.feedpulse.twister
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.feedpulse.twister.Setting
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
