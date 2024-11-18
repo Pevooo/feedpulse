8e946331153d0a777362ece1220e6e4c7100ea3a
Pavly <pavlysamuelatef@gmail.com>
2024-11-19 00:45:20 +0200

    Fix build error

--- twister/app/src/main/java/com/feedpulse/twister/Menu.kt
+++ twister/app/src/main/java/com/feedpulse/twister/Menu.kt
@@ -1,6 +1,5 @@
 package com.feedpulse.twister
 
-import DropdownInput
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
