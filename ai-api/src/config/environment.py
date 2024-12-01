9f2c2d6f46dae81c2db2c0ff955d5c356a9fb3f4
Pavly <pavlysamuelatef@gmail.com>
2024-12-01 20:42:27 +0200

    Add GPT4o-mini Model

--- ai-api/src/config/environment.py
+++ ai-api/src/config/environment.py
@@ -7,6 +7,7 @@ class Environment:
     """
 
     gemini_api_key: str = os.getenv("GEMINI_API_KEY")
+    openai_api_key: str = os.getenv("OPENAI_API_KEY")
     is_production_environment: bool = os.getenv("PROD") is not None
 
     # X related variables
---
+++ ai-api/src/models/openai_model.py
@@ -0,0 +1,24 @@
+import google.generativeai as genai
+import openai
+from src.models.model import Model
+from src.config.environment import Environment
+
+
+class OpenAiModel(Model):
+    """
+    The OpenAI GPT4o-mini model
+    """
+
+    def __init__(self) -> None:
+        openai.api_key = Environment.openai_api_key
+
+    def generate_content(self, text: str):
+        response = openai.ChatCompletion.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "user", "content": text},
+            ],
+            max_tokens=100,
+            temperature=0.7,
+        )
+        return response["choices"][0]["message"]["content"]
