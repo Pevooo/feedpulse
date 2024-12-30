c34c5bf24ef606da99bb020838616e667c7c5919
saaakh63 <sara.khalefa708@compit.aun.edu.eg>
2024-12-30 11:03:05 +0200

    fix angular workflow

--- Interface/adminDashboard/src/karma.conf.js
+++ Interface/adminDashboard/src/karma.conf.js
@@ -7,27 +7,24 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       jasmine: {},
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/adminDashboard'),
       subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }]
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: false,
-    restartOnFileChange: true
+    browsers: ['ChromeHeadless'], // Use ChromeHeadless
+    singleRun: true, // Run tests once
+    restartOnFileChange: true,
   });
 };
