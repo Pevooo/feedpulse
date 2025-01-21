583ae96557870cd799f81b94883a1431885dc43c
af50 <alfredfarag11@gmail.com>
2025-01-21 18:48:34 +0200

    table enum for spark

--- ai-api/src/spark/spark.py
+++ ai-api/src/spark/spark.py
@@ -1,19 +1,28 @@
+import enum
+
+class SparkTable(enum):
+  REPORTS = "reports"
+  INPUT_COMMENTS = "input_comments"
+  PROCESSED_COMMENTS = "processed_comments"
+  PAGES = "pages"
+
+
 class Spark():
   def __new__(cls):
     if not hasattr(cls, 'instance'):
       cls.instance = super(Spark, cls).__new__(cls)
     return cls.instance
   
-  def add(self):
+  def add(self, table: enum, row_data: str):
     pass
   
-  def delete(self):
+  def delete(self, table: enum, row_data: str):
     pass
   
-  def  query(self):
+  def  query(self, table: enum, row_data: str):
     pass
   
-  def modify(self):
+  def modify(self, table: enum, row_data: str):
     pass
 
 
