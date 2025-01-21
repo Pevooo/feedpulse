62f5dc8e38210e1f994c1ee9d571e82412b2bd8b
Alfred Farag <95964035+af50@users.noreply.github.com>
2025-01-21 19:57:20 +0200

    spark creation (#100)

--- ai-api/src/spark/spark.py
+++ ai-api/src/spark/spark.py
@@ -1,45 +1,7 @@
-from enum import Enum
-from pyspark.sql import SparkSession
-from pyspark.sql.types import (
-    StructType,
-    StructField,
-    StringType,
-    TimestampType,
-    ArrayType,
-)
+import enum
 
-# Define schemas
-pages_schema = StructType([StructField("page_id", StringType(), False)])
 
-posts_schema = StructType(
-    [
-        StructField("post_id", StringType(), False),
-        StructField("page_id", StringType(), False),
-        StructField("content", StringType(), True),
-    ]
-)
-
-comments_schema = StructType(
-    [
-        StructField("hashed_comment", StringType(), False),
-        StructField("platform", StringType(), False),
-        StructField("content", StringType(), False),
-        StructField("related_topics", ArrayType(StringType(), True), True),
-        StructField("sentiment", StringType(), True),
-        StructField("transaction_type", StringType(), True),
-        StructField("timestamp", TimestampType(), False),
-    ]
-)
-
-exceptions_schema = StructType(
-    [
-        StructField("exception_id", StringType(), False),
-        StructField("exception_message", StringType(), True),
-        StructField("time", TimestampType(), False),
-    ]
-)
-
-class SparkTable(Enum):
+class SparkTable(enum):
     REPORTS = "reports"
     INPUT_COMMENTS = "input_comments"
     PROCESSED_COMMENTS = "processed_comments"
@@ -52,17 +14,17 @@ class Spark:
             cls.instance = super(Spark, cls).__new__(cls)
         return cls.instance
 
-    def __init__(self):
-        self.spark = SparkSession.builder.appName("session").getOrCreate()
-
-    def add(self, table: SparkTable, row_data: str):
-
-
-    def delete(self, table: SparkTable, row_data: str):
+    def add(self, table: enum, row_data: str):
         pass
 
-    def query(self, table: SparkTable, row_data: str):
+    def delete(self, table: enum, row_data: str):
         pass
 
-    def modify(self, table: SparkTable, row_data: str):
+    def query(self, table: enum, row_data: str):
         pass
+
+    def modify(self, table: enum, row_data: str):
+        pass
+
+
+spark_instance = Spark()
