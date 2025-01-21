a0e1924b2b54badb4428f16a8e46a31e392c32d4
af50 <alfredfarag11@gmail.com>
2025-01-21 19:23:08 +0200

    format modification

--- ai-api/src/spark/spark.py
+++ ai-api/src/spark/spark.py
@@ -1,20 +1,30 @@
-class Spark():
-  def __new__(cls):
-    if not hasattr(cls, 'instance'):
-      cls.instance = super(Spark, cls).__new__(cls)
-    return cls.instance
-  
-  def add(self):
-    pass
-  
-  def delete(self):
-    pass
-  
-  def  query(self):
-    pass
-  
-  def modify(self):
-    pass
+import enum
+
+
+class SparkTable(enum):
+    REPORTS = "reports"
+    INPUT_COMMENTS = "input_comments"
+    PROCESSED_COMMENTS = "processed_comments"
+    PAGES = "pages"
+
+
+class Spark:
+    def __new__(cls):
+        if not hasattr(cls, "instance"):
+            cls.instance = super(Spark, cls).__new__(cls)
+        return cls.instance
+
+    def add(self, table: enum, row_data: str):
+        pass
+
+    def delete(self, table: enum, row_data: str):
+        pass
+
+    def query(self, table: enum, row_data: str):
+        pass
+
+    def modify(self, table: enum, row_data: str):
+        pass
 
 
 spark_instance = Spark()
