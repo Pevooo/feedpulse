379f86accbf42fe478b052a06b7aa4d1d27740e3
af50 <alfredfarag11@gmail.com>
2025-01-21 16:20:55 +0200

    spark creation
    # Conflicts:
    #	ai-api/src/config/response.py
    #	ai-api/src/spark/spark.py

--- ai-api/src/spark/spark.py
+++ ai-api/src/spark/spark.py
@@ -1,30 +1,20 @@
-import enum
-
-
-class SparkTable(enum):
-    REPORTS = "reports"
-    INPUT_COMMENTS = "input_comments"
-    PROCESSED_COMMENTS = "processed_comments"
-    PAGES = "pages"
-
-
-class Spark:
-    def __new__(cls):
-        if not hasattr(cls, "instance"):
-            cls.instance = super(Spark, cls).__new__(cls)
-        return cls.instance
-
-    def add(self, table: enum, row_data: str):
-        pass
-
-    def delete(self, table: enum, row_data: str):
-        pass
-
-    def query(self, table: enum, row_data: str):
-        pass
-
-    def modify(self, table: enum, row_data: str):
-        pass
+class Spark():
+  def __new__(cls):
+    if not hasattr(cls, 'instance'):
+      cls.instance = super(Spark, cls).__new__(cls)
+    return cls.instance
+  
+  def add(self):
+    pass
+  
+  def delete(self):
+    pass
+  
+  def  query(self):
+    pass
+  
+  def modify(self):
+    pass
 
 
 spark_instance = Spark()
