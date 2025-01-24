12f9476f336741f427424cf03a40d78240f70806
Pavly <pavlysamuelatef@gmail.com>
2025-01-24 08:22:56 +0200

    x vars

--- ai-api/src/config/environment.py
+++ ai-api/src/config/environment.py
@@ -10,3 +10,8 @@ class Environment:
     openai_api_key: str = os.getenv("OPENAI_API_KEY")
     is_production_environment: bool = os.getenv("PROD") is not None
     hf_token: str = os.getenv("HF_TOKEN")
+
+    # X related variables
+    x_username: str = os.getenv("X_USERNAME")
+    x_email: str = os.getenv("X_EMAIL")
+    x_password: str = os.getenv("X_PASSWORD")
