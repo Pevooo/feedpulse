e8aed90f35ed8c332897218a66bc91c7c2a009cd
Sara Khalifa <138697141+saaakh63@users.noreply.github.com>
2025-02-17 23:06:52 +0200

    Generic topics (#111)

--- Interface/src/app/app.component.spec.ts
+++ Interface/src/app/app.component.spec.ts
@@ -35,6 +35,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges(); 
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('adminDashboard');
+    expect(compiled.querySelector('title')?.textContent).toContain('adminDashboard');
   });
 });
