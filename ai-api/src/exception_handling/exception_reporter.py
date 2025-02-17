68fa0a74a17b48de99d1a6d22d3d9ff0e93293d5
Mollyamged <115906572+Mollyamged@users.noreply.github.com>
2025-02-17 14:27:01 +0200

    improved_exception_reporter_preformance (#108)

--- ai-api/src/exception_handling/exception_reporter.py
+++ ai-api/src/exception_handling/exception_reporter.py
@@ -7,15 +7,19 @@ from src.spark.spark import Spark, SparkTable
 class ExceptionReporter:
     def __init__(self, spark: Spark):
         self.spark = spark
+        self.exceptions = []
 
     def report(self, exception: Exception):
-        self.spark.add(
-            SparkTable.EXCEPTIONS,
-            [
-                {
-                    "exception_id": str(uuid.uuid4()),
-                    "exception_message": str(exception),
-                    "time": datetime.now().isoformat(),
-                }
-            ],
+        self.exceptions.append(
+            {
+                "exception_id": str(uuid.uuid4()),
+                "exception_message": str(exception),
+                "time": datetime.now().isoformat(),
+            }
         )
+        if len(self.exceptions) == 50:
+            self.spark.add(
+                SparkTable.EXCEPTIONS,
+                self.exceptions,
+            )
+            self.exceptions = []
--- ai-api/test/test_exception_reporter.py
+++ ai-api/test/test_exception_reporter.py
@@ -11,8 +11,25 @@ class TestExceptionReporter(unittest.TestCase):
         self.mock_spark.add = Mock()
         self.exception_reporter = ExceptionReporter(self.mock_spark)
 
-    def test_report_exception(self):
-        self.exception_reporter.report(Exception())
+    def test_one_report_exception(self):
+        self.exception_reporter.report(Exception("Test Exception"))
+
+        self.assertIn(
+            {
+                "exception_id": ANY,
+                "exception_message": "Test Exception",
+                "time": ANY,
+            },
+            self.exception_reporter.exceptions,
+        )
+
+        self.mock_spark.add.assert_not_called()
+
+        self.assertEqual(len(self.exception_reporter.exceptions), 1)
+
+    def test_50_report_exceptions(self):
+        for _ in range(50):
+            self.exception_reporter.report(Exception())
 
         expected_input = [
             {
@@ -20,8 +37,27 @@ class TestExceptionReporter(unittest.TestCase):
                 "exception_message": str(Exception()),
                 "time": ANY,
             }
-        ]
+        ] * 50
 
         self.mock_spark.add.assert_called_once_with(
             SparkTable.EXCEPTIONS, expected_input
         )
+
+        self.assertEqual(len(self.exception_reporter.exceptions), 0)
+
+    def test_more_than_50_report_exceptions(self):
+        for _ in range(51):
+            self.exception_reporter.report(Exception("Test Exception"))
+
+        expected_input = [
+            {
+                "exception_id": ANY,
+                "exception_message": "Test Exception",
+                "time": ANY,
+            }
+        ] * 50
+        self.mock_spark.add.assert_called_once_with(
+            SparkTable.EXCEPTIONS, expected_input
+        )
+
+        self.assertEqual(len(self.exception_reporter.exceptions), 1)
