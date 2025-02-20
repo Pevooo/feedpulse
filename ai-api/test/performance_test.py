a81dedc063eb3ca11cfaff7fb563f6d0409c6787
Pavly <pavlysamuelatef@gmail.com>
2025-02-20 21:24:15 +0200

    Format

--- ai-api/test/performance_test.py
+++ ai-api/test/performance_test.py
@@ -1,32 +1,38 @@
 import time
 from tabulate import tabulate
 
+
 def main():
 
     tests = [
         ("Spark Read 1,000 Rows", _exec_time(spark_read_1k)),
-        ("Spark Read 1,000,000 Rows", _exec_time(spark_read_1m))
+        ("Spark Read 1,000,000 Rows", _exec_time(spark_read_1m)),
     ]
 
     # Generate Markdown table
-    performance_table = tabulate(tests, headers=["Component", "Execution Time (s)"], tablefmt="github")
+    performance_table = tabulate(
+        tests, headers=["Component", "Execution Time (s)"], tablefmt="github"
+    )
     with open("performance_report.md", "w") as f:
         f.write("## 🏎️ Performance Report\n\n")
         f.write(performance_table)
         f.write("\n")
 
+
 def _exec_time(func, *args, **kwargs):
     start = time.perf_counter()
-    func(*args, **kwargs) # Should be synchronous
+    func(*args, **kwargs)  # Should be synchronous
     end = time.perf_counter()
     return end - start
 
+
 def spark_read_1k():
     time.sleep(1.2)
+
 
 def spark_read_1m():
     time.sleep(0.5)
 
 
-if  __name__ == '__main__':
+if __name__ == "__main__":
     main()
