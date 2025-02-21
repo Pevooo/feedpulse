c9dd0090ea8cb1b78cc8bdd4a0cb87e02b52eb53
Aya Abdellatif <ayaabdllatif112@gmail.com>
2025-02-21 16:33:25 +0200

    polling modifications

--- ai-api/src/data_providers/facebook_data_provider.py
+++ ai-api/src/data_providers/facebook_data_provider.py
@@ -34,43 +34,44 @@ class FacebookDataProvider(DataProvider):
         return data.get("id")
 
     @deprecated
-    def get_posts(self) -> tuple[ContextDataUnit, ...]:
+    def get_posts(self) -> tuple[dict, ...]:
         """
-        Gets the posts from a Facebook page using the page access token.
+        Gets the last 100 posts with all of their comments and replies from a Facebook page using the page access token.
 
         Returns:
-            tuple[ContextDataUnit, ...]: A tuple containing all the collected posts as ContextDataUnit objects.
+            tuple[dict, ...]: A tuple containing all the collected comments and replies as dictionaries.
         """
         page_id = self.get_page_id()
         url = f"{FACEBOOK_GRAPH_URL}{page_id}"
         params = {
             "access_token": self.access_token,
-            "fields": "posts{comments,message,created_time}",
+            "fields": "posts.limit(100){id,message,created_time,comments.limit(100){id,message,created_time,comments.limit(100){id,message,created_time}}}"
         }
 
         data = requests.get(url, params, timeout=3).json()
-        posts: list[ContextDataUnit] = []
+        posts: list[dict] = []
         for post_data in data.get("posts", {}).get("data", []):
-            message: str = post_data.get("message", "")
+            #posts
+            post_id = post_data.get("id")
 
-            time_created: datetime = datetime.strptime(
-                post_data["created_time"], DATETIME_FORMAT
-            )
+            #comments
+            for comment_data in post_data.get("comments", {}).get("data", []):
+                posts.append({
+                    "comment_id": comment_data.get("id"),
+                    "post_id": post_id,
+                    "message": comment_data.get("message"),
+                    "created_time": datetime.strptime(comment_data.get("created_time"), DATETIME_FORMAT),
+                    "platform": "facebook"
+                })
 
-            comments: list[FeedbackDataUnit] = (
-                [
-                    FeedbackDataUnit(
-                        comment_data["message"],
-                        datetime.strptime(
-                            comment_data["created_time"], DATETIME_FORMAT
-                        ),
-                        tuple(),
-                    )
-                    for comment_data in post_data.get("comments", {}).get("data", [])
-                ]
-                if post_data.get("comments")
-                else tuple()
-            )
+                #replies
+                for reply_data in comment_data.get("comments", {}).get("data", []):
+                posts.append({
+                    "comment_id": reply_data.get("id"),
+                        "post_id": post_id,
+                        "message": reply_data.get("message"),
+                        "created_time": datetime.strptime(reply_data.get("created_time"), DATETIME_FORMAT),
+                        "platform": "facebook"
+                })
 
-            posts.append(ContextDataUnit(message, time_created, tuple(comments)))
         return tuple(posts)
