ee6eb8e1754fa1c247a630cb50197b1856c5d74c
Aya Abdellatif <ayaabdllatif112@gmail.com>
2025-03-07 15:38:07 +0200

    insta-polling

--- ai-api/src/data_providers/instagram_data_provider.py
+++ ai-api/src/data_providers/instagram_data_provider.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 from typing_extensions import deprecated
 from src.data_providers.data_provider import DataProvider
@@ -36,42 +37,77 @@ class InstagramDataProvider(DataProvider):
         raise Exception("No Instagram Business Account linked to this page.")
 
     # @deprecated
-    # def get_posts(self) -> tuple[ContextDataUnit, ...]:
-    #     """
-    #     Gets the Instagram posts from the linked Instagram Business Account.
-    #
-    #     Returns:
-    #         tuple[ContextDataUnit, ...]: A tuple containing all the collected posts as ContextDataUnit objects.
-    #     """
-    #     instagram_account_id = self.get_instagram_account_id()
-    #     url = f"{FACEBOOK_GRAPH_URL}{instagram_account_id}/media"
-    #     params = {
-    #         "access_token": self.access_token,
-    #         "fields": "caption,timestamp,comments{username,text,timestamp}",
-    #     }
-    #
-    #     data = requests.get(url, params, timeout=3).json()
-    #
-    #     posts: list[ContextDataUnit] = []
-    #     for post_data in data.get("data", []):
-    #         message: str = post_data.get("caption", "")
-    #
-    #         time_created: datetime = datetime.strptime(
-    #             post_data["timestamp"], DATETIME_FORMAT
-    #         )
-    #
-    #         comments: list[FeedbackDataUnit] = (
-    #             [
-    #                 FeedbackDataUnit(
-    #                     comment_data["text"],
-    #                     datetime.strptime(comment_data["timestamp"], DATETIME_FORMAT),
-    #                     tuple(),
-    #                 )
-    #                 for comment_data in post_data.get("comments", {}).get("data", [])
-    #             ]
-    #             if post_data.get("comments")
-    #             else tuple()
-    #         )
-    #
-    #         posts.append(ContextDataUnit(message, time_created, tuple(comments)))
-    #     return tuple(posts)
+    def get_posts(self) -> tuple[dict, ...]:
+        """
+        Gets the Instagram posts from the linked Instagram Business Account along with
+        all of their comments and replies.
+    
+         Returns:
+             tuple[dict, ...]: A tuple containing all the collected comments and replies as dictionaries.
+        """
+        instagram_account_id = self.get_instagram_account_id()
+        url = f"{FACEBOOK_GRAPH_URL}{instagram_account_id}/media"
+        params = {
+            "access_token": self.access_token,
+            "fields": "posts.limit(100){id,caption,created_time,comments.limit(100){id,username,text,created_time,replies.limit(100){id,username,text,created_time}}",
+        }
+    
+        data = requests.get(url, params, timeout=3).json()
+    
+        posts: list[dict] = []
+        for post_data in data.get("data", []):
+            post_id = post_data.get("id")
+
+            #comments
+            comments_edge = post_data.get("comments", {})   
+            comments = comments_edge.get("data", [])
+
+            if "paging" in comments_edge and "next" in comments_edge["paging"]:
+                comments += self.next(comments_edge["paging"]["next"])
+
+            for comment_data in comments:
+                posts.append(
+                    {
+                        "comment_id": comment_data.get("id"),
+                        "post_id": post_id,
+                        "content": comment_data.get("caption"),
+                        "created_time": datetime.strptime(
+                            comment_data.get("created_time"), DATETIME_FORMAT
+                        ),
+                        "platform": "instagram",
+                    }
+                )
+
+                #replies
+                if "replies" in comments:
+
+                    replies_edge = comment_data.get("replies", {})
+                    replies = replies_edge.get("data", [])
+
+                    if "paging" in replies_edge and "next" in replies_edge["paging"]:
+                        replies += self.next(replies_edge["paging"]["next"])
+
+                    for reply_data in replies:
+                        posts.append(
+                            {
+                                "comment_id": reply_data.get("id"),
+                                "post_id": post_id,
+                                "content": reply_data.get("caption"),
+                                "created_time": datetime.strptime(
+                                    comment_data.get("created_time"), DATETIME_FORMAT
+                                ),
+                                "platform": "instagram",
+                            }
+                        )     
+
+        return tuple(posts)
+
+    def next(self, url: str) -> list[dict]:
+        """Fetch all items by following pagination cursors."""
+        items = []
+        while url:
+            response = requests.get(url, params=None, timeout=3).json()
+            items.extend(response.get("data", []))
+            # Once the first page is fetched, subsequent requests use the 'next' URL directly.
+            url = response.get("paging", {}).get("next")
+        return items
