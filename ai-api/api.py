cfcbb304b4f164efce2b018937707daa9552ad44
Pavly <99624292+Pevooo@users.noreply.github.com>
2025-03-28 15:37:34 +0200

    Improve Report Fetching (#165)

--- ai-api/api.py
+++ ai-api/api.py
@@ -49,7 +49,9 @@ class FeedPulseAPI:
 
     def run(self):
         self.data_manager.start_streaming_job()  # Will run on another thread
-        self.flask_app.run(host="0.0.0.0", port=5000, threaded=True)  # Will run on the main thread
+        self.flask_app.run(
+            host="0.0.0.0", port=5000, threaded=True
+        )  # Will run on the main thread
 
     def __setup_exception_reporter(self):
         @self.flask_app.errorhandler(Exception)
