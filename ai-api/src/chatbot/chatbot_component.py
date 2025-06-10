ce10564b93c49e73fdf10f69983046a228c6707f
Aya Abdellatif <ayaabdllatif112@gmail.com>
2025-06-10 22:58:12 +0300

    Remove unused imports

--- ai-api/src/chatbot/chatbot_component.py
+++ ai-api/src/chatbot/chatbot_component.py
@@ -1,13 +1,11 @@
 from abc import ABC, abstractmethod
 
-from src.models.model_type import ModelType
-from src.models.prompt import Prompt
 
 class ChatBotComponent(ABC):
     """
     An abstract base class representing a chatbot interface.
     """
-    
+
     @abstractmethod
-    def run(self, text:str, dataset):
+    def run(self, text: str, dataset):
         pass
--- ai-api/src/chatbot/routing_component.py
+++ ai-api/src/chatbot/routing_component.py
@@ -5,11 +5,15 @@ from chatbot import ChatBot
 from sql_component import SqlComponent
 from visualization_component import VisualizationComponent
 
+
 class Routing:
     def __init__(self, input_text, model_provider: GlobalModelProvider):
         self.model_provider = model_provider
         self.input_text = input_text
-        self.instructions = """
+
+    def run(self):
+        prompt = Prompt(
+            instructions="""
         You will be given a statement. Classify it into one of the following contexts by responding with only the corresponding number:
         1 — General conversation
         2 — SQL query
@@ -17,19 +21,18 @@ class Routing:
         4 — Irrelevant or not understandable
 
         Please respond with only one number (1, 2, 3, or 4).
-        """
-
-    def run(self):
-        prompt = Prompt(
-            instructions=self.instructions,
+        """,
             context=None,
             examples=[
-                ("How many complaints were received about water issues last month?", "2"),
+                (
+                    "How many complaints were received about water issues last month?",
+                    "2",
+                ),
                 ("Hello, how are you!", "1"),
                 ("Show me a chart of the most common complaint types this year.", "3"),
                 ("asdf234@@!!", "4"),
             ],
-            input_text=self.input_text
+            input_text=self.input_text,
         )
 
         response = self.model_provider.generate_content(prompt).strip()
