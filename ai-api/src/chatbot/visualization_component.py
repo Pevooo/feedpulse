cc735b41fa45146dbc0078cb31b4827b70f1e6cf
Aya Abdellatif <ayaabdllatif112@gmail.com>
2025-06-11 15:26:56 +0300

    Remove converting to df

--- ai-api/src/chatbot/visualization_component.py
+++ ai-api/src/chatbot/visualization_component.py
@@ -3,7 +3,6 @@ from lida import Manager, TextGenerationConfig
 from src.reports.custom_text_generator import CustomTextGenerator
 from src.models.global_model_provider import GlobalModelProvider
 from lida.datamodel import Goal
-
 import pandas as pd
 
 
@@ -15,20 +14,12 @@ class VisualizationComponent(ChatBotComponent):
         self.lida = Manager(text_gen=self.text_generator)
         self.config = TextGenerationConfig(n=1, temperature=0.5)
 
-    def run(self, input_text, dataset):
-        if isinstance(dataset, pd.DataFrame):
-            df = dataset
-        elif "toPandas" in dir(dataset):
-            try:
-                df = dataset.toPandas()
-            except Exception as e:
-                raise ValueError(f"Failed to convert Spark DataFrame: {e}")
-        else:
-            raise ValueError(
-                "Unsupported dataset format. Provide a pandas DataFrame, or Spark DataFrame."
-            )
-
-        summary = self.lida.summarize(df, summary_method="default")
-        goal = Goal(question=input_text, rationale=input_text, visualization="auto")
+    def run(self, input_text: str, dataset: pd.DataFrame):
+        summary = self.lida.summarize(dataset, summary_method="default")
+        goal = Goal(
+            question=input_text,
+            rationale="Generate a relevant chart",
+            visualization="auto",
+        )
         code = self.lida.visualize(summary=summary, goal=goal)
         return code
