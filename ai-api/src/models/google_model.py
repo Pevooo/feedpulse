90f6f668f3a344022c33d80bce202f1dd04b7ce1
Pavly <pavlysamuelatef@gmail.com>
2025-06-17 16:01:58 +0300

    Use gemini 2.5 instead of gemini 2.0

--- ai-api/src/models/google_model.py
+++ ai-api/src/models/google_model.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class GoogleModel(Enum):
-    DEFAULT = "gemini-2.0-flash"
+    DEFAULT = "gemini-2.5-flash-preview-05-20"
+    GEMINI_2_5_FLASH = "gemini-2.5-flash-preview-05-20"
     GEMINI_2_0_FLASH = "gemini-2.0-flash"
     GEMINI_1_5_FLASH = "gemini-1.5-flash"
