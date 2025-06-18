e2dc65b4a25ee6c8ae18966465af334078a6c125
Aya Abdellatif <ayaabdllatif112@gmail.com>
2025-06-18 06:55:04 +0300

    Change spinner

--- Interface/src/Components/spinner/spinner.component.ts
+++ Interface/src/Components/spinner/spinner.component.ts
@@ -8,7 +8,7 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <div *ngIf="isLoading$ | async" class="spinner-overlay">
-      <div class="spinner"></div>
+      <img src="assets/gif/spinner.gif" alt="Loading..." class="spinner-gif">
     </div>
   `,
   styles: [`
@@ -18,23 +18,15 @@ import { CommonModule } from '@angular/common';
       left: 0;
       width: 100%;
       height: 100%;
-      background: rgba(0, 0, 0, 0.5);
+      background: rgba(10, 38, 64, 0.7); /* semi-transparent #0a2640 */
       display: flex;
       justify-content: center;
       align-items: center;
       z-index: 9999;
     }
-    .spinner {
-      width: 50px;
-      height: 50px;
-      border: 5px solid #f3f3f3;
-      border-top: 5px solid #3498db;
-      border-radius: 50%;
-      animation: spin 1s linear infinite;
-    }
-    @keyframes spin {
-      0% { transform: rotate(0deg); }
-      100% { transform: rotate(360deg); }
+    .spinner-gif {
+      width: 120px;
+      height: 120px;
     }
   `]
 })
