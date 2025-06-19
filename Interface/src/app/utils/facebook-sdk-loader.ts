dfd79bdd018d68ec528bb19215ddd28885579ea9
Pavly <pavlysamuelatef@gmail.com>
2025-06-19 22:37:36 +0300

    Fix Lint

--- Interface/src/app/utils/facebook-sdk-loader.ts
+++ Interface/src/app/utils/facebook-sdk-loader.ts
@@ -1,24 +1,33 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const FB: any;
 
+declare global {
+  interface Window {
+    fbAsyncInit?: () => void;
+  }
+}
+
 export function initFacebookSDK(): void {
-  (window as any).fbAsyncInit = function () {
-    FB.init({
-      appId: '1213236544138432',
-      cookie: true,
-      xfbml: true,
-      version: 'v22.0'
-    });
-    FB.AppEvents.logPageView();
+  window.fbAsyncInit = () => {
+    if (typeof FB !== 'undefined') {
+      FB.init({
+        appId: '1213236544138432',
+        cookie: true,
+        xfbml: true,
+        version: 'v22.0',
+      });
+      FB.AppEvents.logPageView();
+    }
   };
 
-  const script = document.createElement('script');
-  script.src = 'https://connect.facebook.net/en_US/sdk.js';
-  script.async = true;
-  script.defer = true;
-  script.crossOrigin = 'anonymous';
-  script.id = 'facebook-jssdk';
-
-  if (!document.getElementById('facebook-jssdk')) {
+  const scriptId = 'facebook-jssdk';
+  if (!document.getElementById(scriptId)) {
+    const script = document.createElement('script');
+    script.id = scriptId;
+    script.src = 'https://connect.facebook.net/en_US/sdk.js';
+    script.async = true;
+    script.defer = true;
+    script.crossOrigin = 'anonymous';
     document.body.appendChild(script);
   }
 }
