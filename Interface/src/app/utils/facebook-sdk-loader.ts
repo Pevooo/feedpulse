0e06da550c3499a1f292c5bf41b958fd68f73bf9
Pavly <pavlysamuelatef@gmail.com>
2025-06-19 21:52:36 +0300

    Fix

--- Interface/src/app/utils/facebook-sdk-loader.ts
+++ Interface/src/app/utils/facebook-sdk-loader.ts
@@ -1,11 +1,7 @@
-declare global {
-  interface Window {
-    fbAsyncInit: () => void;
-  }
-}
+declare const FB: any;
 
 export function initFacebookSDK(): void {
-  window.fbAsyncInit = function () {
+  (window as any).fbAsyncInit = function () {
     FB.init({
       appId: '1213236544138432',
       cookie: true,
@@ -22,7 +18,6 @@ export function initFacebookSDK(): void {
   script.crossOrigin = 'anonymous';
   script.id = 'facebook-jssdk';
 
-  // Only append if not already added
   if (!document.getElementById('facebook-jssdk')) {
     document.body.appendChild(script);
   }
