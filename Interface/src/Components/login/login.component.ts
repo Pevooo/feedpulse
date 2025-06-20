94e8dd303cae5dedb0d8cb39d877636970d917dc
Pavly <99624292+Pevooo@users.noreply.github.com>
2025-06-20 16:52:14 +0300

    Fix rtl in home and integrate language in login (#229)

--- Interface/src/Components/login/login.component.ts
+++ Interface/src/Components/login/login.component.ts
@@ -13,7 +13,7 @@ import {
 
 import Swal from 'sweetalert2';
 
-import { TranslateModule } from '@ngx-translate/core';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
 
 import { SignInModel } from '../../app/interfaces/ISignInModel';
 import { AuthService } from '../../app/services/auth.service';
@@ -26,52 +26,45 @@ import { AuthService } from '../../app/services/auth.service';
   styleUrl: './login.component.css'
 })
 export class LoginComponent  {
-Model:SignInModel={userName: " ",password:""};
+Model: SignInModel={ userName: "", password: "" };
 
-constructor(private authService: AuthService,private router:Router) {
+constructor(private authService: AuthService,private router:Router, private translate: TranslateService) {
 
 }
-onSubmit(){
-  //form;
-this.authService.login(this.Model.userName,this.Model.password).subscribe({
-  next:(response)=>{
-    if(response.succeeded&&response.data){
-      localStorage.setItem('accessToken', response.data.accessToken);
-      localStorage.setItem('refreshToken', response.data.refreshToken.token);
+onSubmit() {
+  this.authService.login(this.Model.userName, this.Model.password).subscribe({
+    next: (response) => {
+      if (response.succeeded && response.data) {
+        localStorage.setItem('accessToken', response.data.accessToken);
+        localStorage.setItem('refreshToken', response.data.refreshToken.token);
 
-
-
-       // const decoded = jwtDecode(response.data.accessToken) as JwtPayload;
-        //console.log('decoded',decoded);
-
-
-      console.log('Access Token:', response.data.accessToken);
-      console.log('Refresh Token:', response.data.refreshToken);
-      Swal.fire({
-        icon: 'success',
-        title: 'Login Successful',
-        text: 'Welcome back!',
-        timer: 1500,
-        showConfirmButton: false
-      }).then(() => {
-        this.router.navigate(['/']);
+        this.translate.get(['LOGIN_SUCCESS', 'WELCOME_BACK']).subscribe(translations => {
+          Swal.fire({
+            icon: 'success',
+            title: translations['LOGIN_SUCCESSFUL'],
+            text: translations['WELCOME_BACK'],
+            timer: 1500,
+            showConfirmButton: false
+          }).then(() => {
+            this.router.navigate(['/']);
+          });
+        });
+      } else {
+        console.log('LoginFailed: ' + response.message);
+      }
+    },
+    error: (err) => {
+      this.translate.get(['LOGIN_FAILED', 'INVALID_CREDENTIALS', 'OK']).subscribe(translations => {
+        Swal.fire({
+          icon: 'error',
+          title: translations['LOGIN_FAILED'],
+          text: err.error.message || translations['INVALID_CREDENTIALS'],
+          confirmButtonColor: '#20c0bd',
+          confirmButtonText: translations['OK']
+        });
       });
-    }else{
-      console.log("LoginFaild1"+response.message);
     }
-  },
-  error:(err)=>{
-    console.log(err);
-    Swal.fire({
-      icon: 'error',
-      title: 'Login Failed',
-      text: err.error.message || 'Invalid credentials.',
-      confirmButtonColor: '#20c0bd', // Change this to your desired color
-      confirmButtonText: 'OK' // Explicitly set the button text if needed
-    });
-  }
-})
-
+  });
 }
 
 }
